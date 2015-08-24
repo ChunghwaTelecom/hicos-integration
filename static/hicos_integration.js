e5ac5841f1c4c172489a1ac0409260ac7f59fd84
Pei-Tang Huang <tangtheone@gmail.com>
2015-08-24 14:46:03 +0800

    Format and minor refactor.

--- static/hicos_integration.js
+++ static/hicos_integration.js
@@ -1,57 +1,56 @@
 (function (angular) {
     'use strict';
 
-    var hiCosIntegratoinModule = angular.module('HiCosIntegratoinModule', ['HiCosNativeMessagingModule', 'HiCosAppletModule', 'ng.deviceDetector']);
-		
-	angular.module('HiCosIntegratoinModule')
+    angular.module('HiCosIntegratoinModule', ['HiCosNativeMessagingModule', 'HiCosAppletModule', 'ng.deviceDetector']);
+
+    angular.module('HiCosIntegratoinModule')
         .factory('hiCosService', ['$injector', 'deviceDetector', hiCosService]);
 
-	function hiCosService($injector, deviceDetector) {	
-		
-		var status;
-		var version;
-		var hiCosInstance;
-		var isRunApplet = true;
-		
-		//抄eps的
-		function isJavaAvailable() {
-			var javaRegex = /(Java)(\(TM\)| Deployment)/,
-				plugins = navigator.plugins;
-			if (navigator && plugins) {
-				for (var plugin in plugins){
-					if(plugins.hasOwnProperty(plugin) &&
-							javaRegex.exec(plugins[plugin].name)) {
-						return true;
-					}
-				}
-			}
-			return false;
-		}
-		
-		if (deviceDetector.browser == "chrome") {
-			var chromeVersion = window.navigator.userAgent.match(/Chrome\/(\d+)\./);
-			if (chromeVersion && chromeVersion[1]) {
-				if (parseInt(chromeVersion[1], 10)  >= 42 && !isJavaAvailable()){
-					isRunApplet = false;
-				}
-			}
+    function hiCosService($injector, deviceDetector) {
 
-		} 
+        var status;
+        var version;
+        var hiCosInstance;
+        var isRunApplet = true;
 
-		if(isRunApplet){
-			hiCosInstance = $injector.get("hiCosApplet");
-		}else{
-			hiCosInstance = $injector.get("hiCosNativeMessaging");
-		}
-		
-		return {
-			getStatus: function () { 
-							console.log(status);
-							return status; 
-						},
-			getBrowser: deviceDetector.browser,
-			getVersionInfo:  hiCosInstance.getVersionInfo,
-			getSlotIdListWithToken: hiCosInstance.getSlotIdListWithToken,
+        //抄 eps 的
+        function isJavaAvailable() {
+            var javaRegex = /(Java)(\(TM\)| Deployment)/,
+                plugins = navigator.plugins;
+            if (navigator && plugins) {
+                for (var plugin in plugins) {
+                    if (plugins.hasOwnProperty(plugin) &&
+                        javaRegex.exec(plugins[plugin].name)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        if (deviceDetector.browser == "chrome") {
+            var chromeVersion = window.navigator.userAgent.match(/Chrome\/(\d+)\./);
+            if (chromeVersion && chromeVersion[1]) {
+                if (parseInt(chromeVersion[1], 10) >= 42 && !isJavaAvailable()) {
+                    isRunApplet = false;
+                }
+            }
+        }
+
+        if (isRunApplet) {
+            hiCosInstance = $injector.get("hiCosApplet");
+        } else {
+            hiCosInstance = $injector.get("hiCosNativeMessaging");
+        }
+
+        return {
+            getStatus: function () {
+                console.log(status);
+                return status;
+            },
+            getBrowser: deviceDetector.browser,
+            getVersionInfo: hiCosInstance.getVersionInfo,
+            getSlotIdListWithToken: hiCosInstance.getSlotIdListWithToken,
             getSlotInfoWithToken: hiCosInstance.getSlotInfoWithToken,
             getTokenInfo: hiCosInstance.getTokenInfo,
             getPemX509Certificate: hiCosInstance.getPemX509Certificate,
@@ -60,7 +59,7 @@
             makePemLoginSignedMessage: hiCosInstance.makePemLoginSignedMessage,
             verifySignature: hiCosInstance.verifySignature,
             getLoginInfoFromPemSignedData: hiCosInstance.getLoginInfoFromPemSignedData
-		};
-	};
+        };
+    };
 
 }(angular));
--- static/hicos_native_messaging.js
+++ static/hicos_native_messaging.js
@@ -11,98 +11,82 @@
     var hiCosNativeMessaging = function ($q, $timeout, $log, $rootScope, base64) {
         function getVersionInfo() {
             var message = {"f": "getVersionInfo"};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getSlotIdListWithToken() {
             var message = {"f": "getSlotIdListWithToken"};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getSlotInfoWithToken(slotId) {
-			var message = {"f": "getSlotInfoWithToken", "slotId": slotId};
-			var promise = dispatchMessage(message);
-			return promise
+            var message = {"f": "getSlotInfoWithToken", "slotId": slotId};
+            return dispatchMessage(message)
         }
 
         function getTokenInfo(slotId) {
             var message = {"f": "getTokenInfo", "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getTokenId(slotId) {
             var message = {"f": "getTokenId", "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getPemX509Certificate(pin, slotId, certType) {
             var message = {"f": "getPemX509Certificate", "pin": pin, "slotId": slotId, "certType": certType};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getPemSignatureCertficate(pin, slotId) {
             var message = {"f": "getPemSignatureCertficate", "pin": pin, "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getPemEncryptedCertficate(pin, slotId) {
             var message = {"f": "getPemEncryptedCertficate", "pin": pin, "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getJsonX509Certificate(pin, certType) {
             var message = {"f": "getJsonX509Certificate", "pin": pin, "certType": certType};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getJsonSignatureCertficate(pin) {
             var message = {"f": "getJsonSignatureCertficate", "pin": pin};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getJsonEncryptedCertficate(pin) {
             var message = {"f": "getJsonEncryptedCertficate", "pin": pin};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function checkLoginValid(pin, idn) {
             var message = {"f": "checkLoginValid", "pin": pin, "idn": idn};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function makePemLoginSignedMessage(pin, slotId) {
             var message = {"f": "makePemLoginSignedMessage", "pin": pin, "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function verifySignature(pemSignedData) {
             var message = {"f": "verifySignature", "pemSignedData": pemSignedData};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function getLoginInfoFromPemSignedData(pemSignedData) {
             var message = {"f": "getLoginInfoFromPemSignedData", "pemSignedData": pemSignedData};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function makeBase64PKCS1Signature(tbs, pin, slotId) {
             var message = {"f": "makeBase64PKCS1Signature", "tbs": tbs, "pin": pin, "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function verifyBase64PKCS1Signature(data, base64SignBytes, pin, slotId) {
@@ -113,14 +97,12 @@
                 "pin": pin,
                 "slotId": slotId
             };
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function makeBase64PKCS1RawData(tbs, pin, slotId) {
             var message = {"f": "makeBase64PKCS1RawData", "tbs": tbs, "pin": pin, "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function decryptBase64PKCS1RawData(base64EncryptedData, pin, slotId) {
@@ -130,26 +112,22 @@
                 "pin": pin,
                 "slotId": slotId
             };
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function makePemPKCS7Signature(tbs, pin, slotId) {
             var message = {"f": "makePemPKCS7Signature", "tbs": tbs, "pin": pin, "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function verifyPemPKCS7Signature(pemSignedData) {
             var message = {"f": "verifyPemPKCS7Signature", "pemSignedData": pemSignedData};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function makePKCS7EnvelopedData(tbs, pin, slotId) {
             var message = {"f": "makePKCS7EnvelopedData", "tbs": tbs, "pin": pin, "slotId": slotId};
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function decryptPemPKCS7EnvelopedData(pemEnvelopedData, pin, slotId) {
@@ -159,8 +137,7 @@
                 "pin": pin,
                 "slotId": slotId
             };
-            var promise = dispatchMessage(message);
-            return promise;
+            return dispatchMessage(message);
         }
 
         function dispatchMessage(message) {
@@ -173,104 +150,104 @@
                 functionId: message.f,
                 deferred: deferred
             };
-			
-			var timeoutPromise = $timeout(function () {
-				if (correls[messageId]) {
-				    deferred.reject("作業逾時");
-				    delete correls[messageId];
-				}
-			}, 10000);
-			correls[messageId].timeoutPromise = timeoutPromise;
 
-			var event = new CustomEvent(
-				"hicosreq",
-				{
-					detail: {
-						message: message
-					},
-					bubbles: true,
-					cancelable: true
-				}
-			);
-			document.dispatchEvent(event);
-			
+            var timeoutPromise = $timeout(function () {
+                if (correls[messageId]) {
+                    deferred.reject("作業逾時");
+                    delete correls[messageId];
+                }
+            }, 10000);
+            correls[messageId].timeoutPromise = timeoutPromise;
+
+            var event = new CustomEvent(
+                "hicosreq",
+                {
+                    detail: {
+                        message: message
+                    },
+                    bubbles: true,
+                    cancelable: true
+                }
+            );
+            document.dispatchEvent(event);
+
             return deferred.promise;
         }
 
-		document.addEventListener("hicosresp", function (response) {
-			var message = response.detail;
-			$log.log("Received message (" + message.correlId + "): " + message.code + "-" + message.desc);
-            
-			if (correls[message.correlId]) {
-				var functionId = correls[message.correlId].functionId;
-				var deferred = correls[message.correlId].deferred;
-				
-				var timeoutPromise = correls[message.correlId].timeoutPromise;
-				if (timeoutPromise) {
-					$timeout.cancel(timeoutPromise);
-				}
+        document.addEventListener("hicosresp", function (response) {
+            var message = response.detail;
+            $log.log("Received message (" + message.correlId + "): " + message.code + "-" + message.desc);
 
-				delete correls[message.correlId];
+            if (correls[message.correlId]) {
+                var functionId = correls[message.correlId].functionId;
+                var deferred = correls[message.correlId].deferred;
 
-				var value;
-				switch (functionId) {
-					case "getSlotIdListWithToken":
-						var slots = JSON.parse(message.rtn);
-						/* 這裡沒有做勝淵範例裡的 slots 數目檢查，一來是為了要符名 function 名稱，二來為了跟 applet 版本相容。 */
-						value = slots;
-						break;
+                var timeoutPromise = correls[message.correlId].timeoutPromise;
+                if (timeoutPromise) {
+                    $timeout.cancel(timeoutPromise);
+                }
 
-					case "getVersionInfo":
-					case "getSlotInfoWithToken":
-					case "getTokenInfo":
-					case "makeBase64PKCS1Signature":
-					case "verifyBase64PKCS1Signature":
-					case "makeBase64PKCS1RawData":
-					case "makePemPKCS7Signature":
-					case "verifyPemPKCS7Signature":
-					case "makePKCS7EnvelopedData":
-					case "decryptPemPKCS7EnvelopedData":
+                delete correls[message.correlId];
+
+                var value;
+                switch (functionId) {
+                    case "getSlotIdListWithToken":
+                        var slots = JSON.parse(message.rtn);
+                        /* 這裡沒有做勝淵範例裡的 slots 數目檢查，一來是為了要符名 function 名稱，二來為了跟 applet 版本相容。 */
+                        value = slots;
+                        break;
+
+                    case "getVersionInfo":
+                    case "getSlotInfoWithToken":
+                    case "getTokenInfo":
+                    case "makeBase64PKCS1Signature":
+                    case "verifyBase64PKCS1Signature":
+                    case "makeBase64PKCS1RawData":
+                    case "makePemPKCS7Signature":
+                    case "verifyPemPKCS7Signature":
+                    case "makePKCS7EnvelopedData":
+                    case "decryptPemPKCS7EnvelopedData":
                     case "getPemX509Certificate":
-					case "getPemSignatureCertficate":
-					case "getPemEncryptedCertficate":
-					case "checkLoginValid":
-					case "makePemLoginSignedMessage":
-					case "verifySignature":
-					case "getTokenId":
-						// 可以直接使用的回傳值
-						value = message.rtn;
-						break;
+                    case "getPemSignatureCertficate":
+                    case "getPemEncryptedCertficate":
+                    case "checkLoginValid":
+                    case "makePemLoginSignedMessage":
+                    case "verifySignature":
+                    case "getTokenId":
+                        // 可以直接使用的回傳值
+                        value = message.rtn;
+                        break;
 
-					case "decryptBase64PKCS1RawData":
+                    case "decryptBase64PKCS1RawData":
                     case "getJsonX509Certificate":
-					case "getJsonSignatureCertficate":
-					case "getJsonEncryptedCertficate":
-					case "getLoginInfoFromPemSignedData":
-						// 需 base64 解碼的回傳值
-						value = base64.decode(message.rtn);
-						break;
+                    case "getJsonSignatureCertficate":
+                    case "getJsonEncryptedCertficate":
+                    case "getLoginInfoFromPemSignedData":
+                        // 需 base64 解碼的回傳值
+                        value = base64.decode(message.rtn);
+                        break;
 
-					default:
-						$rootScope.$apply(function () {
-							deferred.reject("不明的功能識別碼: " + functionId);
-						});
-				}
-				
-				$rootScope.$apply(function () {
-					var result = {
-							code: message.code,
-							message: message.desc,
-							value: value
-						};
+                    default:
+                        $rootScope.$apply(function () {
+                            deferred.reject("不明的功能識別碼: " + functionId);
+                        });
+                }
 
-					if (message.code === "0x00000000") {
-						deferred.resolve(result);
-					} else {
-						deferred.reject(result);
-					}
-				});
-			}
-		});
+                $rootScope.$apply(function () {
+                    var result = {
+                        code: message.code,
+                        message: message.desc,
+                        value: value
+                    };
+
+                    if (message.code === "0x00000000") {
+                        deferred.resolve(result);
+                    } else {
+                        deferred.reject(result);
+                    }
+                });
+            }
+        });
 
         return {
             getVersionInfo: getVersionInfo,
