3cfcffb01de969f1b4495d3b90059781fa766441
chiahsi.chang <chiahsi.chang@gmail.com>
2016-08-08 10:44:02 +0800

    與 beta 討論後，統一回傳空陣列

--- static/chrome/hicos_native_messaging.js
+++ static/chrome/hicos_native_messaging.js
@@ -311,7 +311,7 @@
 							/* 這裡沒有做勝淵範例裡的 slots 數目檢查，一來是為了要符名 function 名稱，二來為了跟 applet 版本相容。 */
 							value = slots;
 						} catch (exception) {
-							value = -1;
+							value = [];
 						}
 						
                         break;
